8e7d09ce9e5bc5fa54693004ca88adacf3e5bd52
SKY-LEO <leonid311203@gmail.com>
2022-01-11 19:07:04 +0300

    Update LabaratoryWork4.cpp

--- LabaratoryWork4.cpp
+++ LabaratoryWork4.cpp
@@ -1,20 +1,22 @@
-#include <math.h>
+//#include <math.h>
 #include <iostream>
 #include <conio.h>
 #include <iomanip>
 #define PI 3.141592
 #define E 2.718281
 using namespace std;
-typedef double (*ToFunc)(double, double);
-double funS(double, double);
-double funY(double, double);
-double funZ(double, double);
-void Out_Rez(ToFunc, double, double, double, double);
+typedef double (*ToFunction)(double, int);
+double functionS(double x, int n);
+double functionY(double x, int n);
+double functionZ(double x, int n);
+void Out_Rez(ToFunction, double a, double b, double h, int n);
 double Correct_Input();
+int Correct_Input_Int();
 
 int main()
 {
-	double a, b, h, n;
+	double a, b, h;
+	int n;
 	char code;
 	do {
 		cout << "Please, write a = ";
@@ -24,13 +26,13 @@ int main()
 		cout << "Please, write h = ";
 		h = Correct_Input();
 		cout << "Please, write n = ";
-		n = Correct_Input();
+		n = Correct_Input_Int();
 		cout << "Function Y\n";
-		Out_Rez(funY, a, b, h, n);
+		Out_Rez(functionY, a, b, h, n);
 		cout << "Function S\n";
-		Out_Rez(funS, a, b, h, n);
+		Out_Rez(functionS, a, b, h, n);
 		cout << "|Function Y - Function S|\n";
-		Out_Rez(funZ, a, b, h, n);
+		Out_Rez(functionZ, a, b, h, n);
 		cout << "Do you want to continue? Y/N" << endl;
 		code = _getch();
 	} while (code == 'Y' || code == 'y');
@@ -50,38 +52,57 @@ double Correct_Input()
 		{
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Error, please write numbers!\n" << "Try again!" << endl;
+			cout << "Error, please write number!\n" << "Try again!" << endl;
 		}
 	}
 	return a;
 }
 
-double funS(double x, double n)
+int Correct_Input_Int()
 {
-	double r, resultS;
+	int a;
+	while (true)
+	{
+		cin >> a;
+		if (cin.get() == '\n') {
+			break;
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Error, please write int number!\n" << "Try again!" << endl;
+		}
+	}
+	return a;
+}
+
+double functionS(double x, int n)
+{
+	double recurrent, resultS;
 	resultS = 0;
-	r = x;
+	recurrent = x;
 	for (int k = 1; k <= n; k++) {
-		r *= x;
-		resultS += r * cos(k * PI / 3.) / (k * x);
+		recurrent *= x;
+		resultS += recurrent * cos(k * PI / 3.) / (k * x);
 	}
 	return resultS;
 }
 
-double funY(double x, double n)
+double functionY(double x, int n)
 {
-	return (-1. / 2. * log(1 - 2 * x * cos(PI / 3.) + x * x) / log(E));
+	return (-1. / 2. * log(1. - 2. * x * cos(PI / 3.) + x * x) / log(E));
 }
 
-double funZ(double x, double n)
+double functionZ(double x, int n)
 {
-	return (fabs(funY(x, n) - funS(x, n)));
+	return (fabs(functionY(x, n) - functionS(x, n)));
 }
 
-void Out_Rez(ToFunc f, double a, double b, double h, double n)
+void Out_Rez(ToFunction function, double a, double b, double h, int n)
 {
 	for (double x = a; x <= b; x += h)
 	{
-		cout << "x = " << x << setw(15) << "y = " << f(x, n) << endl;
+		cout << "x = " << x << setw(15) << "y = " << function(x, n) << endl;
 	}
 }
